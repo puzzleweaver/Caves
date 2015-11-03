9c8e6e9bde4b646bab2104387099f24fc3386ff9
Riley <rileyborgard@gmail.com>
2015-11-03 16:19:54 -0500

    Made the main menu look prettier with the new font

--- src/menus/MainMenu.java
+++ src/menus/MainMenu.java
@@ -1,8 +1,6 @@
 package menus;
 
 import java.awt.Font;
-import java.awt.FontFormatException;
-import java.io.IOException;
 import java.io.InputStream;
 
 import main.Input;
@@ -31,7 +29,7 @@ public class MainMenu implements Menu {
 		try {
 			fipps = Font.createFont(Font.TRUETYPE_FONT, inputStream);
 			fippsTTF = new TrueTypeFont(fipps.deriveFont(18f), false);
-			fippsTTFBig = new TrueTypeFont(fipps.deriveFont(40f), false);
+			fippsTTFBig = new TrueTypeFont(fipps.deriveFont(24f), false);
 		} catch (Exception e) {}
 	}
 	
@@ -40,22 +38,28 @@ public class MainMenu implements Menu {
 		for(int i = 0; i <= Main.w/32+1; i++) {
 			for(int j = 0; j <= Main.h/32+1; j++) {
 				Image stoneTex = Sprite.textures[0]; //change this however necessary to make it look smoother
-				g.drawImage(stoneTex, i*32-offset, j*32-offset);
+				g.drawImage(stoneTex, i*32, j*32-offset);
 			}
 		}
 		//title
-		g.drawImage(Sprite.title, Main.w/2-Sprite.title.getWidth()/2, Main.h/4-Sprite.title.getHeight()/2);
+		//g.drawImage(Sprite.title, Main.w/2-Sprite.title.getWidth()/2, Main.h/4-Sprite.title.getHeight()/2);
 		g.setFont(fippsTTFBig);
+		g.setColor(Color.black);
+		g.drawString("Caves", Main.w/2 - fippsTTFBig.getWidth("Caves")/2, Main.h/4);
 		//play button
-		g.drawImage(Sprite.playButton[isPlayButtonHovered() ? 1 : 0], Main.w/2-Sprite.title.getWidth()/2, Main.h/2-Sprite.title.getHeight()/2);
+		g.setColor(isPlayButtonHovered() ? Color.red : Color.blue);
+		g.drawString("Play", Main.w/2 - fippsTTFBig.getWidth("Play")/2, Main.h/2);
+		;
 		//name
 		g.setFont(fippsTTF);
 		g.setColor(Color.black);
-		g.drawString("Name:", Main.w/2 - (fippsTTF.getWidth("Name:") / 2), 3*Main.h/4);
+		g.drawString("Name:", Main.w/2 - fippsTTF.getWidth("Name:")/2, 3*Main.h/4);
+		g.setColor(Color.green.darker(0.75f));
 		g.drawString(name, Main.w/2 - (fippsTTF.getWidth(name) / 2), 3*Main.h/4 + 30);
 		//generate button
-		g.setColor(isGenerateButtonHovered() ? new Color(0, 102, 153) : new Color(0, 75, 112));
-		g.drawString("Generate", Main.w/2 - (fippsTTF.getWidth("Generate") / 2), 3*Main.h/4 + 60);
+		Color color = new Color(0, 102, 153);
+		g.setColor(isGenerateButtonHovered() ? color : color.darker(0.5f));
+		g.drawString("Generate", Main.w/2 - fippsTTF.getWidth("Generate")/2, 3*Main.h/4 + 60);
 	}
 	
 	public void update() {
@@ -72,16 +76,16 @@ public class MainMenu implements Menu {
 	private boolean isPlayButtonHovered() {
 		int mx = Input.mouseX();
 		int my = Input.mouseY();
-		int bx = Main.w/2 - Sprite.title.getWidth()/2;
-		int by = Main.h/2 - Sprite.title.getHeight()/2;
-		return mx >= bx && mx <= bx+192 && my >= by && my <= by+64;
+		int bx = Main.w/2 - fippsTTFBig.getWidth("Play")/2;
+		int by = Main.h/2;
+		return mx >= bx && mx <= bx+fippsTTFBig.getWidth("Play") && my >= by && my <= by+fippsTTFBig.getHeight("Play");
 	}
 	private boolean isGenerateButtonHovered() {
 		int mx = Input.mouseX();
 		int my = Input.mouseY();
-		int bx = Main.w/2 - 50;
+		int bx = Main.w/2 - fippsTTF.getWidth("Generate")/2;
 		int by = 3*Main.h/4 + 60;
-		return mx >= bx && mx <= bx+100 && my >= by && my <= by+20;
+		return mx >= bx && mx <= bx+fippsTTF.getWidth("Generate") && my >= by && my <= by+fippsTTF.getHeight("Generate");
 	}
 	
 }
